3daaad3119abb5847fad0330d81d52352c1c1fa5
Hector Mendoza <hmendoza@24hourfit.com>
2015-03-16 18:39:03 -0600

    Adding catalogs enum

---
+++ src/main/java/org/rage/ticket/model/enu/Catalogs.java
@@ -0,0 +1,20 @@
+package org.rage.ticket.model.enu;
+
+
+/**
+ * Catalog represents ...
+ *
+ * @author hector.mendoza
+ * @version $Id$
+ * @since 11/03/2015
+ *
+ */
+public enum Catalogs
+{
+   /** Catalog for REPORT */
+   REPORT, /** Catalog for PRIORITY */
+   PRIORITY, /** Catalog for AREA */
+   AREA, /** Catalog for SERVICE_TYPE */
+   SERVICE_TYPE, /** Catalog for STATUS */
+   STATUS;
+}
